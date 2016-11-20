5e853fe9921832958899e4598e3bf04310881366
Roberto Cánovas <rcanovas.corp@gmail.com>
2016-11-20 05:05:48 +0100

    Ruta añadida: -Route con la ruta root de la API -Controlador para gestionar esta peticion

---
+++ app/controllers/RootController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.*;
+import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class RootController extends Controller {
+
+  public Result index() {
+    ObjectNode result = Json.newObject();
+    result.put("root", "API working well");
+    result.put("API_doc", "http://localhost:9000/api/doc");
+    return ok(result);
+  }
+
+}

b572747f1e837d418ed2b63003bf09f8081ba9ca
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-01-09 23:25:45 +0100

    Añadidos campos a la entidad Serie

--- app/models/Serie.java
+++ app/models/Serie.java
@@ -2,6 +2,10 @@ package models;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import play.data.validation.Constraints;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -21,8 +25,14 @@ public class Serie {
   @Column(columnDefinition = "text")
   public String overview;
   public String banner;
+  public String poster;
+  public String fanart;
   @Column(length = 50)
   public String network;
+  public Integer runtime;
+  @ElementCollection
+  public Set<String> genre = new HashSet();
+  public String rating;
   // NOTE: error con H2 en test @Column(columnDefinition = "enum('Continuing', 'Ended')")
   @Enumerated(EnumType.STRING)
   public Status status;
@@ -32,14 +42,21 @@ public class Serie {
 
   // contructor por campos
   public Serie(Integer idTVDB, String seriesName, Date firstAired,
-              String overview, String banner, String network, Status status) {
+              String overview, String banner, String poster, String fanart,
+              String network, Integer runtime, Set<String> genre,
+              String rating, Status status) {
 
     this.idTVDB = idTVDB;
     this.seriesName = seriesName;
     this.firstAired = firstAired;
     this.overview = overview;
     this.banner = banner;
+    this.poster = poster;
+    this.fanart = fanart;
     this.network = network;
+    this.runtime = runtime;
+    this.genre = genre;
+    this.rating = rating;
     this.status = status;
   }
 
@@ -50,7 +67,12 @@ public class Serie {
     this.firstAired = serie.firstAired;
     this.overview = serie.overview;
     this.banner = serie.banner;
+    this.poster = serie.poster;
+    this.fanart = serie.fanart;
     this.network = serie.network;
+    this.runtime = serie.runtime;
+    this.genre = serie.genre;
+    this.rating = serie.rating;
     this.status = serie.status;
   }
 
@@ -58,7 +80,7 @@ public class Serie {
   public String toString() {
     return "Serie [id=" + id + ", idTVDB=" + idTVDB + ", seriesName="
             + seriesName + ", firstAired=" + firstAired + ", overview="
-            + overview + ", banner=" + banner + ", network=" + network
+            + overview + ", network=" + network
             + ", status=" + status + "]";
   }
 }

c52be5b23d864ce60a3381be13d9a901a31c5fee
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-05-18 17:25:07 +0200

    Vista serie añadida

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -3,10 +3,15 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.administration.index;
+import views.html.administration.tvShows;
 
 public class AdminController extends Controller {
 
   public Result index() {
-    return ok(index.render("Hola mundo"));
+    return ok(index.render("Hola mundo", "dashboard"));
+  }
+
+  public Result tvShows() {
+    return ok(tvShows.render("Hola mundo", "tvShows"));
   }
 }

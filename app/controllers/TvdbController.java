17df76cc4d6665bb14333d1bc38184db63e76346
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-05-22 19:38:55 +0200

    Controlador TVDB para devolver por ruta una serie

---
+++ app/controllers/TvdbController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.inject.Inject;
+import json.TvShowViews;
+import models.TvShow;
+import models.service.tvdb.TvdbService;
+import play.db.jpa.Transactional;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class TvdbController extends Controller {
+
+  private final TvdbService tvdbService;
+
+  @Inject
+  public TvdbController(TvdbService tvdbService) {
+    this.tvdbService = tvdbService;
+  }
+
+  // buscar TV Show en TVDB por id
+  @Transactional(readOnly = true)
+  public Result tvShowById(Integer tvdbId) {
+
+    TvShow tvShow = tvdbService.findOnTvdbByTvdbId(tvdbId);
+    if (tvShow != null) {
+      try {
+        JsonNode jsonNode = Json.parse(new ObjectMapper()
+                .writerWithView(TvShowViews.SearchTVDB.class)
+                .writeValueAsString(tvShow));
+        return ok(jsonNode);
+
+      } catch (Exception ex) {
+        // si hubiese un error, devolver error interno
+        ObjectNode result = Json.newObject();
+        result.put("error", "It can't be processed");
+        return internalServerError(result);
+      }
+    } else {
+      ObjectNode result = Json.newObject();
+      result.put("error", "Not found");
+      return notFound(result);
+    }
+
+  }
+}

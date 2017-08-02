03b87174138afe42b8a75d1b6605921a1b2b6539
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-08-02 04:01:28 +0200

    TVDB - obtener serie, obtener imagenes, + mejoras Se ha implementado la función principal getTvShowTVDB que obtiene de TVDB una serie, persistiendo en nuetra base de datos todos los datos que requerimos Se ha implementado la función parseDate para intentar parsear la fecha que nos devuelve TVDB cuando le pedimos en cualquier función una serie, ya que en casos concretos necesita de más de un intento para que el parseo se lleve a cabo por causas todavía desconocidas. Como solución temporal, se reintenta un número determinado de veces este parseo Se han implementado una serie de funciones que se encargan de descargar las imágenes de cada tipo de las series ya que necesiten de peticiones a la API de TVDB para cada una.
    Se ha modificado la petición general de cualquier petición a TVDB para mejorarla y abstraerla lo máximo posible:
    - Idioma de la petición - primero cualquier petición la realizamos en español para descargar todos los datos posibles (incluyendo la imágenes) en español; si no fuera posible, se vuelve a realizar la petición en inglés
    - No autorizado - en situaciones muy concretas nuestra identificación actual en TVDB no es correcta, por lo que llamamos a login y se prueba de nuevo a realizar la petición
    - Repetir petición - en casos concretos la petición a TVDB falla por lo que repetimos la acción una vez más

--- app/models/service/tvdb/TvdbService.java
+++ app/models/service/tvdb/TvdbService.java
@@ -7,9 +7,17 @@ import play.Logger;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSResponse;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 
@@ -26,7 +34,8 @@ public class TvdbService {
     this.tvdbConnection = tvdbConnection;
   }
 
-  private JsonNode tvdbGetRequest(String query) {
+  // peticion a TVDB
+  private JsonNode tvdbGetRequest(String query, String language) {
     JsonNode result = null;
     int MAXTRIES = 2; // maximos intentos de refrescar token
     int count = 0;
@@ -35,7 +44,7 @@ public class TvdbService {
       try {
         CompletionStage<JsonNode> stage = ws.url(query)
                 .setHeader("Authorization", "Bearer " + tvdbConnection.getToken())
-                .setHeader("Accept-Language", "es")
+                .setHeader("Accept-Language", language)
                 .get()
                 .thenApply(WSResponse::asJson);
 
@@ -45,17 +54,28 @@ public class TvdbService {
           if (result.has("data")) {
             break;
           } else if (result.has("Error") && Objects.equals(result.get("Error").asText(), "Not authorized")) {
-            System.out.println("Sin autorizacion, refrescando token...");
+            // no autorizado en tvdb
+            Logger.info("Petición TVDB - Sin autorizacion, refrescando token...");
             tvdbConnection.refreshToken();
+          } else if (result.has("Error") && result.get("Error").asText().contains("No results")) {
+            // error: query sin resultados
+            if (language.equals("es")) {
+              Logger.info("Petición TVDB - sin resultados - probando en inglés...");
+              return tvdbGetRequest(query, "en");
+            } else {
+              Logger.info("Petición TVDB - sin resultados, no podemos obtener datos de la siguiente query:\nQuery: " + query);
+            }
           } else {
-            System.out.println("TVDB error que desconocemos");
+            Logger.error(result.toString());
+            Logger.debug("Petición TVDB - error que desconocemos");
           }
         } else {
-          System.out.println("Respuesta es null o llevamos 2 intentos");
+          Logger.info("Petición TVDB - respuesta es null o llevamos 2 intentos, no podemos obtener datos de la " +
+                  "siguiente query:\nQuery: " + query);
           break;
         }
       } catch (Exception ex) {
-        System.out.println("TVDB exception: " + ex.getMessage());
+        Logger.error("Petición TVDB - " + ex.getMessage());
         break;
       }
     }
@@ -63,13 +83,80 @@ public class TvdbService {
     return result;
   }
 
+  // descargar una imagen desde una url a un directorio local definido por tvdbId y tipo
+  private String downloadImage(URL url, String path) {
+    BufferedImage image;
+    String format = "jpg";
+    try {
+      // descargamos imagen
+      image = ImageIO.read(url);
+      File imageFile = new File(path);
+      // creamos carpetas
+      imageFile.getParentFile().mkdirs();
+      // guardamos imagen
+      ImageIO.write(image, format, imageFile);
+    } catch (Exception ex) {
+      Logger.error("Download Image - error descargando imagen");
+      return null;
+    }
+    return path;
+  }
+
+  // obtener imagen por tvdbId y tipo
+  public String getImage(TvShow tvShow, String type) {
+    Logger.info(tvShow.name + " - descargando " + type);
+    String imageQuery = "https://api.thetvdb.com/series/" + tvShow.tvdbId.toString() + "/images/query?keyType=" + type;
+
+    try {
+      JsonNode images = tvdbGetRequest(imageQuery, "es").withArray("data");
+
+      String fileName = images.get(0).get("fileName").asText();
+
+      // descargar imagen
+      URL downloadURL = new URL("http://thetvdb.com/banners/" + fileName);
+      String path = "./public/images/series/" + tvShow.id.toString() + "/" + type + ".jpg";
+      String resultPath = downloadImage(downloadURL, path);
+      if (resultPath != null) {
+        Logger.info(tvShow.name + " - " + type + " descargado");
+        return resultPath;
+      }
+    } catch (Exception ex) {
+      Logger.error(tvShow.name + " - error descargando " + type);
+      Logger.debug(ex.getClass().toString());
+    }
+    return null;
+  }
+
+  // obtener banner - función específica por características especiales de banner...
+  public String getBanner(TvShow tvShow) {
+    Logger.info(tvShow.name + " - descargando banner");
+    try {
+      if (!tvShow.banner.isEmpty()) {
+        URL downloadURL = new URL("http://thetvdb.com/banners/" + tvShow.banner);
+        String path = "./public/images/series/" + tvShow.id.toString() + "/banner.jpg";
+        String resultPath = downloadImage(downloadURL, path);
+        if (resultPath != null) {
+          Logger.info(tvShow.name + " - banner descargado");
+          return resultPath;
+        }
+      } else {
+        Logger.info(tvShow.name + " - no tiene banner");
+      }
+    } catch (MalformedURLException ex) {
+      Logger.error(tvShow.name + " Error formato URL de banner, no descargado");
+    } catch (Exception ex) {
+      Logger.error(tvShow.name + " Error descargando banner: " + ex.getMessage());
+    }
+    return null;
+  }
+
   // buscar por tvdbId (solo un resultado posible)
   public TvShow findOnTvdbByTvdbId(Integer tvdbId) {
     TvShow tvShow = null;
-    JsonNode respuesta = null;
+    JsonNode respuesta;
     String query = "https://api.thetvdb.com/series/" + tvdbId.toString();
 
-    respuesta = tvdbGetRequest(query);
+    respuesta = tvdbGetRequest(query, "es");
 
     // comprobamos si ha encontrado el tv show en Tvdb
     if (respuesta != null && respuesta.has("data")) {
@@ -78,25 +165,16 @@ public class TvdbService {
 
       // inicializamos el tv show
       tvShow =  new TvShow();
-      tvShow.tvdbId = Integer.parseInt(jsonTvShow.get("id").asText()); // id de tvdbConnection
+      tvShow.tvdbId = jsonTvShow.get("id").asInt();                    // id de tvdbConnection
+      tvShow.imdbId = jsonTvShow.get("imdbId").asText();
       tvShow.name = jsonTvShow.get("seriesName").asText();             // nombre de la tvShow
       tvShow.banner = jsonTvShow.get("banner").asText();               // banner de la tvShow
       tvShow.local = false;
       JsonNode fecha = jsonTvShow.get("firstAired");
-
-      // parsear fecha
-      while (true) {
-        try {
-          df.applyPattern("yyyy-MM-dd");
-          tvShow.firstAired = df.parse(fecha.asText()); // fecha estreno
-          break;
-        } catch (Exception e) {
-          Logger.debug("Reintentando parsear fecha de estreno");
-        }
-      }
+      tvShow.firstAired = parseDate(fecha);
 
     } else {
-      Logger.debug("TvShow no encontrada en TVDB");
+      Logger.info("TvShow no encontrada en TVDB");
     }
 
     return tvShow;
@@ -105,11 +183,11 @@ public class TvdbService {
   // buscar por campo
   public List<TvShow> findOnTVDBby(String field, String value) {
     // buscamos el TV Show en tvdbConnection
-    JsonNode respuesta = null;
+    JsonNode respuesta;
     List<TvShow> tvShows = new ArrayList<>();
     String query = "https://api.thetvdb.com/search/series?" + field + "=" + value;
 
-    respuesta = tvdbGetRequest(query);
+    respuesta = tvdbGetRequest(query, "es");
 
     // recorremos tv shows encontrados en TVDB
     if (respuesta != null && respuesta.has("data")) {
@@ -120,12 +198,9 @@ public class TvdbService {
         nuevaTvShow.name = jsonTvShow.get("seriesName").asText();   // nombre del TV Show
         nuevaTvShow.banner = jsonTvShow.get("banner").asText();     // banner del TV Show
         nuevaTvShow.local = false;                                  // iniciamos por defecto a false
-        try {
-          df.applyPattern("yyyy-MM-dd");
-          nuevaTvShow.firstAired = df.parse(jsonTvShow.get("firstAired").asText()); // fecha estreno
-        } catch (ParseException e) {
-          Logger.debug("No se ha podido parsear la fecha de estreno");
-        }
+
+        JsonNode fecha = jsonTvShow.get("firstAired");
+        nuevaTvShow.firstAired = parseDate(fecha);
 
         // finalmente la añadimos a la lista
         tvShows.add(nuevaTvShow);
@@ -133,6 +208,74 @@ public class TvdbService {
     }
 
     return tvShows;
+  }
+
+  public TvShow getTvShowTVDB(Integer tvdbId) {
+    TvShow tvShow = null;
+    JsonNode respuesta;
+    String query = "https://api.thetvdb.com/series/" + tvdbId.toString();
+
+    respuesta = tvdbGetRequest(query, "es");
+
+    // comprobamos si ha encontrado el tv show en Tvdb
+    if (respuesta != null && respuesta.has("data")) {
+      JsonNode jsonTvShow = respuesta.with("data");
+      Logger.info("TvShow encontrado en TvdbConnection: " + jsonTvShow.get("seriesName").asText());
+
+      // inicializamos el tv show
+      tvShow =  new TvShow();
+      tvShow.tvdbId = jsonTvShow.get("id").asInt();                    // id de tvdbConnection
+      tvShow.imdbId = jsonTvShow.get("imdbId").asText();
+      tvShow.name = jsonTvShow.get("seriesName").asText();             // nombre de la tvShow
+      tvShow.banner = jsonTvShow.get("banner").asText();               // banner de la tvShow
+      tvShow.network = jsonTvShow.get("network").asText();
+      tvShow.overview = jsonTvShow.get("overview").asText();
+      tvShow.rating = jsonTvShow.get("rating").asText();
+      tvShow.runtime = jsonTvShow.get("runtime").asText();
+      tvShow.local = false;
+      JsonNode fecha = jsonTvShow.get("firstAired");
+      tvShow.firstAired = parseDate(fecha);
+
+      // generos
+      for (JsonNode genre : jsonTvShow.get("genre")) {
+        tvShow.genre.add(genre.toString());
+      }
+
+      // status
+      if (jsonTvShow.get("status").asText().equals(TvShow.Status.Continuing.toString())) {
+        tvShow.status = TvShow.Status.Continuing;
+      } else if (jsonTvShow.get("status").asText().equals(TvShow.Status.Ended.toString())) {
+        tvShow.status = TvShow.Status.Ended;
+      }
+
+    } else {
+      Logger.info("TvShow no encontrada en TVDB");
+    }
+
+    return tvShow;
+  }
+
+  // parsear fecha
+  private Date parseDate(JsonNode jsonDate) {
+    int i = 0, tries = 100;
+    Date result = null;
+
+    while (i < tries) {
+      try {
+        df.applyPattern("yyyy-MM-dd");
+        result = df.parse(jsonDate.asText()); // fecha estreno
+        break;
+      } catch (Exception e) {
+        i++;
+        if (i != 100) {
+          Logger.info("Reintentando parsear fecha de estreno");
+        } else {
+          Logger.error("No se ha podido parsear la fecha");
+        }
+      }
+    }
+
+    return result;
   }
 
 }

3b505f734fa5a9d0b4e6e0a173ee3fb5bc876ddb
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-08-02 06:38:41 +0200

    Peticiones - rechazar petición En este fichero javascript se ha implementado la función de rechazar las peticiones.
    Hace los cambios visuales y efectos en tiempo real para una experiencia de usuario excelente que informa en todo momento de cómo está siendo el rechazo de la petición, si ha funcionado, si no ha funcionado, un resumen de los errores obtenidos, etc.

--- public/javascripts/pages/tvShowRequests.js
+++ public/javascripts/pages/tvShowRequests.js
@@ -152,6 +152,137 @@ $(document).on('click', '[data-action=accept-tvShow]', function(e) {
     });
 });
 
+// rechazar serie
+$(document).on('click', '[data-action=reject-tvShow]', function(e) {
+  console.log('rechazar');
+  e.preventDefault();
+  let requestId = $(this).attr('href');
+  let htmlDataOk = '<i class="icon-checkmark4 text-green"></i>';
+  let htmlDataError = '<i class="icon-cross2 text-danger"></i>';
+
+  $('#requests_table_panel').block({
+    message: '<i class="icon-spinner9 spinner"></i>',
+    overlayCSS: {
+      backgroundColor: '#fff',
+      opacity: 0.8,
+      cursor: 'wait'
+    },
+    css: {
+      border: 0,
+      padding: 0,
+      backgroundColor: 'none'
+    }
+  });
+
+  swal({
+      title: 'Rechazar petición',
+      text: '¿Deseas rechazar esta serie?',
+      type: 'info',
+      showCancelButton: true,
+      closeOnConfirm: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        // cambiar estado
+        $('#status-' + requestId).html('<i class="icon-spinner9 spinner"></i>');
+        // desbloquear tabla
+        $('#requests_table_panel').unblock();
+
+        // hacer peticion
+        var promises2 = [];
+        var data = JSON.stringify({"requestId": requestId});
+        let promise2 = $.ajax({
+          url: 'http://localhost:9000/admin/tvshows/requests',
+          type: 'PATCH',
+          data: data,
+          dataType: 'json',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + window.localStorage.getItem('jwt')
+          },
+          success: function (response) {
+            if (response.ok !== null) {
+
+              // cambiar estado
+              let status = '<ul class="icons-list">' +
+                '<li class="dropdown">' +
+                '<a href="#" class="dropdown-toggle tooltips" data-toggle="dropdown" data-popup="tooltip" title="Resumen de rechazo">' +
+                htmlDataOk +
+                '</a>' +
+                '<div class="dropdown-menu dropdown-content">' +
+                '<div class="dropdown-content-heading">' +
+                'Resumen' +
+                '</div>' +
+                '<ul class="media-list check-list dropdown-content-body">' +
+                '<li>Petición rechazada</li>' +
+                '<li class="divider"></li>' +
+                '<li><button type="button" class="btn btn-labeled btn-xs bg-danger btn-group-justified"><b><i class="icon-tv"></i></b> Ver rechazos</button></li>' +
+                '</ul>' +
+                '</li>' +
+                '</ul>';
+              $('#status-' + requestId).html(status);
+            } else {
+              console.log("ok - error rechazando");
+              // cambiar estado
+              let status = '<ul class="icons-list">' +
+                '<li class="dropdown">' +
+                '<a href="#" class="dropdown-toggle tooltips" data-toggle="dropdown" data-popup="tooltip" title="Resumen de rechazo">' +
+                htmlDataError +
+                '</a>' +
+                '<div class="dropdown-menu dropdown-content">' +
+                '<div class="dropdown-content-heading">' +
+                'Error rechazando petición' +
+                '</div>' +
+                '<ul class="media-list check-list dropdown-content-body">' +
+                '<li>' + response.responseJSON.error + '</li>' +
+                '<li class="divider"></li>' +
+                '<li><button type="button" data-action="get-list" class="btn btn-labeled btn-xs bg-warning btn-group-justified"><b><i class="icon-database-refresh"></i></b> Recargar lista</button></li>' +
+                '</ul>' +
+                '</li>' +
+                '</ul>';
+              $('#status-' + requestId).html(status);
+              $('#requests_table_panel').unblock();
+            }
+          },
+          error: function (response) {
+            console.log("error - error rechazando");
+            // cambiar estado
+            let status = '<ul class="icons-list">' +
+              '<li class="dropdown">' +
+              '<a href="#" class="dropdown-toggle tooltips" data-toggle="dropdown" data-popup="tooltip" title="Resumen de rechazo">' +
+              htmlDataError +
+              '</a>' +
+              '<div class="dropdown-menu dropdown-content">' +
+              '<div class="dropdown-content-heading">' +
+              'Error rechanzando petición' +
+              '</div>' +
+              '<ul class="media-list check-list dropdown-content-body">' +
+              '<li>' + response.responseJSON.error + '</li>' +
+              '<li class="divider"></li>' +
+              '<li><button type="button" data-action="get-list" class="btn btn-labeled btn-xs bg-warning btn-group-justified"><b><i class="icon-database-refresh"></i></b> Recargar lista</button></li>' +
+              '</ul>' +
+              '</li>' +
+              '</ul>';
+            $('#status-' + requestId).html(status);
+            $('#requests_table_panel').unblock();
+          }
+        });
+        promises2.push(promise2);
+
+        $.when.apply(null, promises2).done(function() {
+          $('#requests_table_panel').unblock();
+        });
+      } else {
+        // se ha cancelado el popup
+        $('#requests_table_panel').unblock();
+      }
+
+    });
+});
+
 $(function() {
 
   // Table setup

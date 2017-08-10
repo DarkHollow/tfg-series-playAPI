7aadfa6e60dfcc056d5a2a001a8d564ad6777458
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-08-10 09:14:55 +0200

    Función eliminar serie funcional El botón añadido en la vista de un TV Show en administración de Eliminar serie ya está operativo Se ha añadido el código javascript necesario para ello en su fichero externo js

--- public/javascripts/pages/tvShow.js
+++ public/javascripts/pages/tvShow.js
@@ -233,3 +233,115 @@ $(document).on('click', '[data-action=download-images]', function(e) {
 
     });
 });
+
+// eliminar una serie
+$(document).on('click', '[data-action=delete-tvShow]', function(e) {
+  console.log('eliminar serie');
+  e.preventDefault();
+  let button = $(this);
+  let buttonIcon = $("i", this);
+  let tvShowId = button.attr('data-id');
+  let host = 'http://' + button.attr('data-host');
+  let redirect = button.attr('data-redirect');
+
+  $('#tvShow_panel').block({
+    message: '<i class="icon-spinner9 spinner"></i>',
+    overlayCSS: {
+      backgroundColor: '#fff',
+      opacity: 0.8,
+      cursor: 'wait'
+    },
+    css: {
+      border: 0,
+      padding: 0,
+      backgroundColor: 'none'
+    }
+  });
+
+  swal({
+      title: 'Eliminar serie',
+      text: '¿Seguro que quieres eliminar esta serie? Esta acción no se podrá deshacer.',
+      type: 'warning',
+      showCancelButton: true,
+      closeOnConfirm: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        // cambiar estado
+        buttonIcon.attr('class', 'icon-spinner9 spinner');
+
+        // hacer peticion
+        var promises = [];
+        let promise = $.ajax({
+          url: host + '/api/tvshows/' + tvShowId,
+          type: 'DELETE',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + window.localStorage.getItem('jwt')
+          },
+          success: function (response) {
+            if (response.ok !== null) {
+              console.log('serie eliminada, volver a listado de series');
+              setTimeout(function() {
+                swal({
+                    title: 'Eliminar serie',
+                    text: 'Serie eliminada con éxito',
+                    html: true,
+                    type: 'success',
+                    showCancelButton: false,
+                    closeOnConfirm: true,
+                    confirmButtonText: 'Aceptar'
+                  },
+                  function() {
+                    getRoute(redirect);
+                  });
+              }, 500);
+            } else {
+              console.log("success - error eliminando serie");
+              swal({
+                  title: 'Error',
+                  text: 'No se ha podido la serie en este momento',
+                  type: 'error',
+                  showCancelButton: false,
+                  closeOnConfirm: true,
+                  confirmButtonText: 'Aceptar'
+                },
+                function() {
+                  buttonIcon.attr('class', 'icon-eraser');
+                  $('#tvShow_panel').unblock();
+                });
+            }
+          },
+          error: function (response) {
+            console.log("error - error eliminando serie");
+            setTimeout(function() {
+              swal({
+                  title: 'Error',
+                  text: 'No se ha podido la serie en este momento',
+                  type: 'error',
+                  showCancelButton: false,
+                  closeOnConfirm: true,
+                  confirmButtonText: 'Aceptar'
+                },
+                function() {
+                  buttonIcon.attr('class', 'icon-eraser');
+                  $('#tvShow_panel').unblock();
+                });
+            }, 500);
+          }
+        });
+        promises.push(promise);
+
+        $.when.apply(null, promises).done(function() {
+          // redirigir...
+        });
+      } else {
+        // se ha cancelado el popup
+        $('#tvShow_panel').unblock();
+      }
+
+    });
+});

ae27640121bec98b9eb5d12b824c8ebf0de11489
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-08-16 05:36:02 +0200

    Implementada mostrar aviso de nueva evolution en vista administración En cualquier vista de administración sale un aviso grande rojo avisando de que hay que ejecutar una evolución si la hay Al pulsar en el boton, carga los datos desde una funcion nueva del controlador de Evolutions para mostrar informacion de la evolution Falta implementar funcion que ejecuta dicha evolution

---
+++ app/controllers/EvolutionController.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.inject.Inject;
+import models.Evolution;
+import models.TvShow;
+import models.TvShowRequest;
+import models.service.EvolutionService;
+import models.service.TvShowRequestService;
+import models.service.TvShowService;
+import models.service.UserService;
+import play.Logger;
+import play.db.jpa.Transactional;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Security;
+import utils.Security.Administrator;
+import views.html.administration.index;
+import views.html.administration.login;
+
+import java.util.List;
+
+public class EvolutionController extends Controller {
+  private final EvolutionService evolutionService;
+
+  @Inject
+  public EvolutionController(EvolutionService evolutionService) {
+    this.evolutionService = evolutionService;
+  }
+
+  @Transactional
+  @Security.Authenticated(Administrator.class)
+  public Result getEvolutionsJSON() {
+    ObjectNode result = Json.newObject();
+
+    // información de última versión aplicada
+    Evolution lastApplied = evolutionService.getLastApplied();
+    if (lastApplied != null) {
+      result.put("actualVersion", lastApplied.version.toString());
+    } else {
+      result.put("actualVersion", "0");
+    }
+
+    // información de nueva y última versión
+    List<Evolution> notApplied = evolutionService.getNotApplied();
+    if (!notApplied.isEmpty()) {
+      // si hay evolution no aplicada
+      result.put("newVersion", notApplied.get(notApplied.size() - 1).version.toString());
+
+      // información de actualizaciones necesarias para alcanzar la nueva y última versión
+      try {
+        JsonNode jsonNode = Json.parse(new ObjectMapper().writeValueAsString(notApplied));
+        result.set("evolutions", jsonNode);
+      } catch (JsonProcessingException e) {
+        Logger.error("Error parseando datos lista evolutions a JSON");
+        result.put("evolutions", "error");
+      }
+
+    } else {
+      // si no hay evolution no aplicada
+      result.put("newVersion", "none");
+    }
+
+    return ok(result);
+  }
+
+}
--- public/javascripts/pages/main.js
+++ public/javascripts/pages/main.js
@@ -107,3 +107,86 @@ $(document).on('click', '[data-action=get-list]', function() {
   getRoute(route);
   return false;
 });
+
+// adminMain
+// cargar evolutions
+$(document).on('click', '[data-evolution=load]', function(e) {
+  e.preventDefault();
+  let button = $(this);
+  let host = 'http://' + button.attr('data-host');
+  let modal = $('#evolution_modal');
+  let modalTitle = $('#upgrade-title');
+  // obtener evolutions
+
+  modal.block({
+    message: '<div class="modal-full-rob"><i class="icon-spinner9 spinner"></i> Cargando datos...</div>',
+    overlayCSS: {
+      backgroundColor: '#fff',
+      opacity: 0.8,
+      cursor: 'wait'
+    },
+    css: {
+      border: 0,
+      padding: 0,
+      backgroundColor: 'none'
+    }
+  });
+
+  // pedir datos de evolutions
+  var promises = [];
+  promise = $.ajax({
+    url: host + '/admin/evolutions',
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + window.localStorage.getItem('jwt')
+    },
+    success: function (response) {
+      // comprobamos si hay version nueva
+      if (response.newVersion !== undefined && response.newVersion !== 'none') {
+        let actualVersion = response.actualVersion;
+        let newVersion = response.newVersion;
+
+        modalTitle.html('<i class="icon-server"></i> Actualización necesaria de v' + actualVersion + ' a v' + newVersion);
+
+        var body = '<h6 class="text-semibold">Información</h6>' +
+          '<p>Versión actual: <span id="actual-version">' + actualVersion + '</span></p>' +
+          '<p>Versión nueva: <span id="new-version">'+ newVersion + '</span></p>' +
+          '<hr />' +
+          '<p>Es necesario aplicar esta actualización para el correcto funcionamiento del sistema debido a ' +
+          'recientes actualizaciones. Cuando comience la actualización, espera a que finalice correctamente.</p>' +
+          '<p>Aviso: si no aplicas esta actualización, el sistema no funcionará correctamente y podría dañar los ' +
+          'datos existentes, te recomendamos que actualices antes de realizar ninguna acción.</p>';
+
+        body += '<button type="button" class="btn btn-sm btn-success btn-labeled" data-host="' + button.attr('data-host') + '" data-evolution="upgrade">' +
+          '<b><i class="icon-play4"></i></b>' +
+          'Comenzar actualización' +
+          '</button>';
+
+        $('#upgrade-body').html(body);
+      } else {
+        // no hay actualizaciones que aplicar
+        modalTitle.html('<i class="icon-server"></i> No hay actualizaciones disponibles');
+        body = 'El sistema está actualizado a la última versión.';
+        $('#upgrade-body').html(body);
+      }
+
+    },
+    error: function () {
+      modalTitle.html('<i class="icon-server"></i> Actualización');
+      var body = 'Error obteniendo datos';
+      $('#upgrade-body').html(body);
+    }
+  });
+  promises.push(promise);
+
+  $.when.apply(null, promises).done(function() {
+    modal.unblock();
+  }).fail(function() {
+    modal.unblock();
+  });
+
+  return false;
+});

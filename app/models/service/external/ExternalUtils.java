c8129e3d92d3d7abaa83ce7d14b9f9237d7bf9a6
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-11-15 20:29:28 +0100

    Mensaje del anterior commit

--- app/models/service/external/ExternalUtils.java
+++ app/models/service/external/ExternalUtils.java
@@ -3,13 +3,18 @@ package models.service.external;
 import com.fasterxml.jackson.databind.JsonNode;
 import play.Logger;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class JsonUtils {
+public class ExternalUtils {
 
   // parsear fecha
-  public Date parseDate(JsonNode jsonDate) {
+  Date parseDate(JsonNode jsonDate) {
     int i = 0, tries = 100;
     Date result = null;
     SimpleDateFormat df = new SimpleDateFormat();
@@ -34,4 +39,51 @@ public class JsonUtils {
 
     return result;
   }
+
+  // descargar una imagen desde una url a un directorio local
+  String downloadImage(URL url, String format, String path) {
+    BufferedImage image;
+    try {
+      // descargamos imagen
+      image = ImageIO.read(url);
+      File imageFile = new File(path);
+      // creamos carpetas
+      Boolean foldersCreated = imageFile.getParentFile().mkdirs();
+      Logger.info("Ruta creada: " + foldersCreated);
+      // guardamos imagen
+      ImageIO.write(image, format, imageFile);
+    } catch (Exception ex) {
+      Logger.error("Download Image - error descargando imagen");
+      return null;
+    }
+    return path;
+  }
+
+  // generar mitad de hashCode y positivo
+  int positiveHalfHashCode(int hashCode) {
+    int halfHashCode = hashCode / 2;
+    return Math.abs(halfHashCode);
+  }
+
+  void deleteOldImages(String folderPath, String type, String fileName) {
+    final File folder = new File(folderPath);
+    final File[] files = folder.listFiles(new FilenameFilter() {
+      @Override
+      public boolean accept(final File dir, final String name) {
+        return (name.startsWith(type.substring(0, 1)) && !name.matches(fileName));
+      }
+    });
+
+    if (files != null) {
+      for (final File file: files) {
+        if (!file.delete()) {
+          Logger.error("No se ha podido borrar el fichero " + file.getAbsolutePath());
+        }
+      }
+    }
+
+  }
+
+
+
 }

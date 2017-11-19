1ab87f1ce46f66ae7fdca5344dd1a97bd4e1522b
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-11-19 21:35:20 +0100

    ExternalUtils - nullableString implementado +mejoras

--- app/models/service/external/ExternalUtils.java
+++ app/models/service/external/ExternalUtils.java
@@ -1,12 +1,12 @@
 package models.service.external;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.lang3.StringUtils;
 import play.Logger;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,23 +15,24 @@ public class ExternalUtils {
 
   // parsear fecha
   Date parseDate(JsonNode jsonDate) {
-    int i = 0, tries = 100;
     Date result = null;
-    SimpleDateFormat df = new SimpleDateFormat();
+    if (jsonDate != null) {
+      int i = 0, tries = 100;
+      SimpleDateFormat df = new SimpleDateFormat();
 
-    if (!jsonDate.isNull() && !jsonDate.asText().equals("")) {
-
-      while (i < tries) {
-        try {
-          df.applyPattern("yyyy-MM-dd");
-          result = df.parse(jsonDate.asText()); // fecha estreno
-          break;
-        } catch (Exception e) {
-          i++;
-          if (i != 100) {
-            Logger.info("Reintentando parsear fecha de estreno");
-          } else {
-            Logger.error("No se ha podido parsear la fecha");
+      if (!jsonDate.isNull() && !jsonDate.asText().equals("")) {
+        while (i < tries) {
+          try {
+            df.applyPattern("yyyy-MM-dd");
+            result = df.parse(jsonDate.asText()); // fecha estreno
+            break;
+          } catch (Exception e) {
+            i++;
+            if (i != 100) {
+              Logger.info("Reintentando parsear fecha de estreno");
+            } else {
+              Logger.error("No se ha podido parsear la fecha");
+            }
           }
         }
       }
@@ -41,7 +42,7 @@ public class ExternalUtils {
   }
 
   // descargar una imagen desde una url a un directorio local
-  String downloadImage(URL url, String format, String path) {
+  public String downloadImage(URL url, String format, String path) {
     BufferedImage image;
     try {
       // descargamos imagen
@@ -60,19 +61,15 @@ public class ExternalUtils {
   }
 
   // generar mitad de hashCode y positivo
-  int positiveHalfHashCode(int hashCode) {
+  public int positiveHalfHashCode(int hashCode) {
     int halfHashCode = hashCode / 2;
     return Math.abs(halfHashCode);
   }
 
-  void deleteOldImages(String folderPath, String type, String fileName) {
+  // type: poster - p, fanart - f, banner - b, season poster - sX (X === season number)
+  public void deleteOldImages(String folderPath, String type, String fileName) {
     final File folder = new File(folderPath);
-    final File[] files = folder.listFiles(new FilenameFilter() {
-      @Override
-      public boolean accept(final File dir, final String name) {
-        return (name.startsWith(type.substring(0, 1)) && !name.matches(fileName));
-      }
-    });
+    final File[] files = folder.listFiles((dir, name) -> (name.startsWith(type) && !name.matches(fileName)));
 
     if (files != null) {
       for (final File file: files) {
@@ -84,6 +81,8 @@ public class ExternalUtils {
 
   }
 
-
+  String nullableString(String string) {
+    return StringUtils.isNotEmpty(string) ? string : null;
+  }
 
 }

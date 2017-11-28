c165a4c0cc9dbcf483b2f8630a4f4905068204c8
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-11-28 18:39:14 +0100

    Relación entre Episode y Season

--- app/models/Season.java
+++ app/models/Season.java
@@ -2,12 +2,14 @@ package models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonView;
 import play.data.validation.Constraints;
 import utils.json.JsonViews;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "season")
@@ -37,6 +39,11 @@ public class Season {
   @JsonView(JsonViews.FullAll.class)
   @Column(columnDefinition = "text")
   public String overview;
+
+  @OneToMany(mappedBy = "season", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonManagedReference
+  @JsonView(JsonViews.FullTvShow.class)
+  public List<Episode> episodes;
 
   // constructor vacio
   public Season() {}

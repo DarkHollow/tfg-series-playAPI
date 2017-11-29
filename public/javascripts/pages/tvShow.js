247d3afe918563c4343dc591a0d43271a382e7e8
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-11-29 21:06:43 +0100

    Botón y función javascript para redescargar todos los episodios

--- public/javascripts/pages/tvShow.js
+++ public/javascripts/pages/tvShow.js
@@ -262,7 +262,8 @@ $(document).on('click', '[data-action=download-seasons]', function(e) {
 
   swal({
       title: 'Descargar temporadas',
-      text: '¿Seguro que quieres volver a descargar las temporadas? Lo demás se quedará como está.',
+      text: '¿Seguro que quieres volver a descargar las temporadas? Lo demás se quedará como está.\nEste proceso puede ' +
+      'durar unos minutos dependiendo de la cantidad de temporadas y episodios de la serie.',
       type: 'info',
       showCancelButton: true,
       closeOnConfirm: true,
@@ -311,6 +312,109 @@ $(document).on('click', '[data-action=download-seasons]', function(e) {
               swal({
                   title: 'Error',
                   text: 'No se ha podido descargar los datos de las temporadas en este momento',
+                  type: 'error',
+                  showCancelButton: false,
+                  closeOnConfirm: true,
+                  confirmButtonText: 'Aceptar'
+                },
+                function() {
+                  buttonIcon.attr('class', 'icon-download');
+                  $('#tvShow_panel').unblock();
+                });
+            }, 500);
+          }
+        });
+        promises.push(promise);
+
+        $.when.apply(null, promises).done(function() {
+          // redirigir...
+        });
+      } else {
+        // se ha cancelado el popup
+        $('#tvShow_panel').unblock();
+      }
+
+    });
+});
+
+// volver a descargar los episodios de todas las temporadas de la serie
+$(document).on('click', '[data-action=download-episodes]', function(e) {
+  console.log('redescargar episodios');
+  e.preventDefault();
+  let button = $(this);
+  let buttonIcon = $("i", this);
+  let tvShowId = button.attr('data-id');
+  let host = 'http://' + button.attr('data-host');
+  let redirect = button.attr('data-reload');
+  let htmlDataOk = '<i class="icon-checkmark4 text-green"></i>';
+  let htmlDataError = '<i class="icon-cross2 text-danger"></i>';
+
+  $('#tvShow_panel').block({
+    message: '<i class="icon-spinner9 spinner"></i>',
+    overlayCSS: {
+      backgroundColor: '#fff',
+      opacity: 0.8,
+      cursor: 'wait'
+    },
+    css: {
+      border: 0,
+      padding: 0,
+      backgroundColor: 'none'
+    }
+  });
+
+  swal({
+      title: 'Descargar episodios',
+      text: '¿Seguro que quieres volver a descargar los episodios? Lo demás se quedará como está.\nEste proceso puede ' +
+      'durar unos minutos dependiendo del número de episodios de la serie.',
+      type: 'info',
+      showCancelButton: true,
+      closeOnConfirm: true,
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        // cambiar estado
+        buttonIcon.attr('class', 'icon-spinner9 spinner');
+
+        // hacer peticion
+        var promises = [];
+        let promise = $.ajax({
+          url: host + '/api/tvshows/' + tvShowId,
+          type: 'PUT',
+          data: JSON.stringify({ "update": "seasons" }),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + window.localStorage.getItem('jwt')
+          },
+          success: function (response) {
+            if (response.ok !== null) {
+              console.log('serie actualizada, recargando...');
+              getRoute(redirect);
+            } else {
+              console.log("success - error persistiendo");
+              swal({
+                  title: 'Error',
+                  text: 'No se ha podido descargar los datos de la serie en este momento',
+                  type: 'error',
+                  showCancelButton: false,
+                  closeOnConfirm: true,
+                  confirmButtonText: 'Aceptar'
+                },
+                function() {
+                  buttonIcon.attr('class', 'icon-download');
+                  $('#tvShow_panel').unblock();
+                });
+            }
+          },
+          error: function (response) {
+            console.log("error - error persistiendo");
+            setTimeout(function() {
+              swal({
+                  title: 'Error',
+                  text: 'No se ha podido descargar los datos de los episodios en este momento',
                   type: 'error',
                   showCancelButton: false,
                   closeOnConfirm: true,

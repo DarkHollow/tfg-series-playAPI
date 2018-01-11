1bf4ff3b67818c511acd0b1720fbe629427dbffe
Roberto Cánovas <rcanovas.corp@gmail.com>
2018-01-11 01:26:06 +0100

    Refactorización tests

--- test/service/PopularServiceTest.java
+++ test/service/PopularServiceTest.java
@@ -88,16 +88,16 @@ public class PopularServiceTest {
 
   // testeamos obtener top 10
   @Test
-  public void testPopularServiceGetTop10(){
+  public void testPopularServiceGetPopular(){
     jpa.withTransaction(() -> {
-      List<Popular> top10Popular = popularService.getTop10();
-      assertEquals(2, top10Popular.size());
+      List<Popular> populars = popularService.getPopular(5);
+      assertEquals(2, populars.size());
     });
   }
 
   // testeamos obtener top 10 comprobar ordenacion 1
   @Test
-  public void testPopularServiceGetTop10Order1(){
+  public void testPopularServiceGetPopularOrder1(){
     final TvShowDAO tvShowDAO = new TvShowDAO(jpa);
     jpa.withTransaction(() -> {
       TvShow tvShow1 = tvShowDAO.find(1);
@@ -109,10 +109,10 @@ public class PopularServiceTest {
       tvShow1.popular.requestsCount.set(1, 2000);
       tvShow1.popular.requestsCount.set(2, 3000);
 
-      List<Popular> top10Popular = popularService.getTop10();
+      List<Popular> populars = popularService.getPopular(5);
 
-      assertEquals(1, (int) top10Popular.get(0).id);
-      assertEquals(6000, (int) top10Popular.get(0).getPopularity());
+      assertEquals(1, (int) populars.get(0).id);
+      assertEquals(6000, (int) populars.get(0).getPopularity());
 
       tvShow1.popular.requestsCount.clear();
       tvShow2.popular.requestsCount.clear();
@@ -141,10 +141,10 @@ public class PopularServiceTest {
       tvShow2.popular.requestsCount.set(2, 3000);
       tvShow2.popular.requestsCount.set(3, 1000);
 
-      List<Popular> top10Popular = popularService.getTop10();
+      List<Popular> populars = popularService.getPopular(5);
 
-      assertEquals(2, (int) top10Popular.get(0).id);
-      assertEquals(7000, (int) top10Popular.get(0).getPopularity());
+      assertEquals(2, (int) populars.get(0).id);
+      assertEquals(7000, (int) populars.get(0).getPopularity());
 
       tvShow1.popular.requestsCount.clear();
       tvShow2.popular.requestsCount.clear();
